04d9f68b95d6be3568125e69a7e9056eab8f7cad
승원이 <71477375+tmddmddnjs@users.noreply.github.com>
2021-12-29 20:04:56 +0900

    10871

---
+++ x보다 작은 수.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, x;
+	cin >> n >> x;
+	vector<int> count;
+
+	int input;
+	for (int i = 0; i < n; i++) {
+		cin >> input;
+		count.push_back(input);
+	}
+	for (int i = 0; i < n; i++) {
+		if (count[i] < x) {
+			cout << count[i] << ' ';
+		}
+	}
+}
