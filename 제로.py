eeabb670114f386e8fe70efabdbff111dbe3b2c1
승원이 <71477375+tmddmddnjs@users.noreply.github.com>
2022-02-12 00:11:13 +0900

    Add files via upload

---
+++ 제로.py
@@ -0,0 +1,15 @@
+#10773
+k = int(input(""))
+l = []
+answer, num = 0, 0
+for i in range(k):
+    num = int(input(""))
+    if num == 0:
+        l.pop()
+    else:
+        l.append(num)
+
+# print(sum(l))
+for i in range(len(l)):
+    answer += l[i]
+print(answer)
